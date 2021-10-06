854faf32a4ba1cb6628bc5f06a88dfd7220026e1
shevaughn98 <shevaughnlegall@gmail.com>
2021-10-06 14:45:18 +0000

    another challenge

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+    if(typeof num === "number"){
+        if( Math.sign(num) > 0 ){
+            return num * -1
+    }else{
+        return num
+    }
+    
+    }
 }
 
-
-
+absoluteValue(10.35)
 
